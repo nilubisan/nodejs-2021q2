3ed49d68229df73fc71380a0954a740c340a23ed
Rinat <r.nas9329@gmail.com>
2021-05-29 00:41:23 +0600

    feat: add column model

---
+++ src/resources/boards/column.model.js
@@ -0,0 +1,16 @@
+const { v4: uuidv4 } = require('uuid');
+
+class Column {
+  constructor(column) {
+    this.id = uuidv4();
+    this.title = column.title;
+    this.order = column.order;
+  }
+
+  updateColumn(column) {
+      this.title = column.title || this.title;
+      this.order = column.order || this.order;
+  }
+}
+
+module.exports = Column;
