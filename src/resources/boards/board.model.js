8270d73f3e129132c1cceabca1776fa8ea1b8039
Rinat <r.nas9329@gmail.com>
2021-05-29 22:00:33 +0600

    docs: add JSDoc description to Board model

--- src/resources/boards/board.model.js
+++ src/resources/boards/board.model.js
@@ -1,12 +1,23 @@
 const { v4: uuidv4 } = require('uuid');
-
+/**
+ * Class representing a Board model
+ */
 class Board {
+  /**
+   * Creates Board object
+   * @param {object} Board object 
+   */
   constructor(board) {
     this.id = uuidv4();
     this.title = board.title;
     this.columns = board.columns;
   }
 
+  /**
+   * Checks if passed Board object contains all required properties
+   * @param {*} board 
+   * @returns {boolean} Return true if Board object contains all required properties. Otherwise, return false
+   */
   static validateBoard(board) {
     return (
       board.title &&
@@ -16,6 +27,10 @@ class Board {
     );
   }
 
+  /**
+   * Updates Board object with new property(ies) passed in object 
+   * @param {object} updated board property(ies)
+   */
   updateBoard(updatedBoard) {
     this.title = updatedBoard.title || this.title;
     this.columns = updatedBoard.columns || this.columns;
